ff33c11408f1ff73935cecd555a8d5ef8bf3f838
Paulo Spiguel <paulospil@hotmail.com>
2020-01-11 02:07:00 +0000

    deploy aws

--- backend/src/database/index.js
+++ backend/src/database/index.js
@@ -20,6 +20,17 @@ class Database {
   init() {
     this.connection = new Sequelize(dbConfig);
 
+    if (process.env.NODE_ENV === 'development') {
+      this.connection
+        .authenticate()
+        .then(res => {
+          console.log('Connection has been established successfully.');
+        })
+        .catch(err => {
+          console.log('Unable to connect to the database:', err);
+        });
+    }
+
     models
       .map(model => model.init(this.connection))
       .map(model => model.associate && model.associate(this.connection.models));
